31a01abbeeccfe948438390c147f63cc608b2118
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2021-07-03 14:15:19 +0200

    Pasamos contador a componente manualmente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Component } from '@angular/core';
   // `,
 })
 export class AppComponent {
-  public titulo: string = 'Contador App';
-  public contador: number = 5;
-  public base: number = 5;
-
-  acumular( valor: number): void {
-    this.contador += valor;
-  }
+  
 }
