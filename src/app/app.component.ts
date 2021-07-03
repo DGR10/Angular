7db36c44a7f4d0e2ccd5ca0d66c80adb4243d7dd
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2021-07-03 14:04:18 +0200

    Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Nombre del componente
+  templateUrl: 'app.component.html'
+  // template: `
+  //   <h1>Hola Mundo</h1>
+  // `,
 })
 export class AppComponent {
-  title = 'bases';
+  public titulo: string = 'Contador App';
+  public contador: number = 5;
+  public base: number = 5;
+
+  acumular( valor: number): void {
+    this.contador += valor;
+  }
 }
